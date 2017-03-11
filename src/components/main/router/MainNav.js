82a16cb9b7c5d10b2589824e4a1aa10925b52990
System Administrator <root@gisang-iui-MacBook-Pro.local>
2017-03-11 16:37:10 +0900

    test

--- src/components/main/router/MainNav.js
+++ src/components/main/router/MainNav.js
@@ -19,17 +19,17 @@ export default class MainNav extends React.Component  {
             <Navbar fixedTop default collapseOnSelect >
               <Navbar.Header>
                 <Navbar.Brand>
-                  <Link style={titleStyle} onClick={this.props.handleOnPageChange} to="/">Zinobeen</Link>
+                  <Link style={titleStyle} to="/">Zinobeen</Link>
                 </Navbar.Brand>
                 <Navbar.Toggle />
               </Navbar.Header>
               <Navbar.Collapse>
                 <Nav>
-                  <NavItem componentClass="span" eventKey={1} ><a onClick={this.handleCategoryClick} style={linkColor}>경품정보</a></NavItem>
+                  <NavItem><Link onClick={this.handleCategoryClick} style={linkColor}>경품정보</Link></NavItem>
                 </Nav>
                 <Nav pullRight>
-                  <NavItem componentClass="span" eventKey={1} ><a onClick={this.handleOpinionClick} style={linkColor}>문의 사항</a></NavItem>
-                  <NavItem componentClass="span" eventKey={2} ><a onClick={this.handlePartnerClick} style={linkColor}>광고 문의</a></NavItem>
+                  <NavItem eventKey={1} ><a onClick={this.handleOpinionClick} style={linkColor}>문의 사항</a></NavItem>
+                  <NavItem eventKey={2} ><a onClick={this.handlePartnerClick} style={linkColor}>광고 문의</a></NavItem>
                 </Nav>
               </Navbar.Collapse>
             </Navbar>
