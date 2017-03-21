f1094c8324b536ed4281ba7b3a93e865bfbfaf3e
System Administrator <root@gisang-iui-MacBook-Pro.local>
2017-03-22 00:18:31 +0900

    123

--- src/components/main/TempPage.js
+++ src/components/main/TempPage.js
@@ -1,11 +1,46 @@
 import React from 'react';
 import axios from 'axios';
 import config from '../../config';
+import {PageHeader, Image} from 'react-bootstrap';
+import MediaQuery from 'react-responsive';
+
+
+export const smallImgSize = {
+  width: "auto",
+  height: "300px",
+
+};
+
+export const mediumImgSize = {
+  width: "auto",
+  height: "500px",
+};
+
+export const largeImgSize = {
+  width: "auto",
+  height: "600px",
+};
+
+
 
 const TempPage = ({ params }) => {
 
   return (
-    <div> 사이트에 연결중입니다...! </div>
+    <div>
+      <MediaQuery minDeviceWidth={1025}>
+        <Image style={largeImgSize}  alt="900x500" src="/assets/logo.png"/>
+      </MediaQuery>
+      <MediaQuery maxDeviceWidth={1224}>
+        <MediaQuery maxDeviceWidth={500}>
+          <Image style={smallImgSize}  alt="900x500" src="/assets/logo.png"/>
+        </MediaQuery>
+        <MediaQuery minDeviceWidth={501}>
+          <Image style={mediumImgSize}  alt="900x500" src="/assets/logo.png"/>
+        </MediaQuery>
+    </MediaQuery><br/> <br/>
+    <PageHeader  > 이동중 입니다.....</PageHeader>
+
+    </div>
   );
 }
 
