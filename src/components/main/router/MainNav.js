1a3486d9b687b1727aedfb5cddfbad1ad823789a
System Administrator <root@gisang-iui-MacBook-Pro.local>
2017-03-21 16:04:36 +0900

    123

--- src/components/main/router/MainNav.js
+++ src/components/main/router/MainNav.js
@@ -9,7 +9,7 @@ export default class MainNav extends React.Component  {
 
     render() {
         return(
-            <Navbar fixedTop default collapseOnSelect >
+            <Navbar>
               <Navbar.Header>
                 <Navbar.Brand>
                   <Link style={titleStyle} to="/">Zinobeen</Link>
@@ -17,15 +17,15 @@ export default class MainNav extends React.Component  {
                 <Navbar.Toggle />
               </Navbar.Header>
               <Navbar.Collapse>
-              <Navbar.Text>
-                <Navbar.Link style={linkColor} href="/advertise"> 경품 정보</Navbar.Link>
-              </Navbar.Text>
+                <Navbar.Text>
+                  <Navbar.Link style={linkColor} href="/advertise"> 경품 정보</Navbar.Link>
+                </Navbar.Text>
                 <Navbar.Text pullRight>
                   <Navbar.Link style={linkColor} href="/partner"> 고객 문의</Navbar.Link>
                 </Navbar.Text>
                 <Navbar.Text pullRight>
                   <Navbar.Link style={linkColor} href="/opinion"> 광고 문의</Navbar.Link>
-               </Navbar.Text>
+                </Navbar.Text>
               </Navbar.Collapse>
             </Navbar>
         );
