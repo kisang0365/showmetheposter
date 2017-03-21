2aa34d74f4eedaf142408b4f639838cbb6f086d6
System Administrator <root@gisang-iui-MacBook-Pro.local>
2017-03-21 23:47:45 +0900

    123

--- src/components/main/TempPage.js
+++ src/components/main/TempPage.js
@@ -4,12 +4,8 @@ import config from '../../config';
 
 const TempPage = ({ params }) => {
 
-  axios.get('/api/ad/'+params.id)
-    .then(res => {
-        window.location.href= res.data.ad.url;
-  });
   return (
-    <div>사이트에 연결중입니다...!</div>
+    <div> 사이트에 연결중입니다...! </div>
   );
 }
 
